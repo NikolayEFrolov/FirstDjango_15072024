d1b011176bb64fcfaa310c61d7fab814e2a74300
Nikolay Frolov <frolovne70@gmail.com>
2024-07-17 12:02:42 +0300

    Done. M1:task5-6

--- MainApp/views.py
+++ MainApp/views.py
@@ -47,8 +47,16 @@ def get_item(request, item_id):
     for item in items:
         if item['id'] == item_id:
             result = f"""
-            <h2> Имя: {item["name"]} <h2>
-            <p> Количество: {item["quantity"]} <p>
+            <h2> Имя: {item["name"]} </h2>
+            <p> Количество: {item["quantity"]} </p>
+            <p><a href ='/items'>Назад к списку товаров</a></p>
             """
             return HttpResponse(result)
-    return HttpResponseNotFound(f"Товар с id={item_id} не найден)
+    return HttpResponseNotFound(f"Товар с id={item_id} не найден")
+
+def get_items(request):
+    result = "<h1>Список товаров</h1><ol>"
+    for item in items:
+        result += f"""<li><a href ='/item/{item["id"]}'>{item["name"]}</li>"""
+    result += "</ol>"
+    return HttpResponse(result)
