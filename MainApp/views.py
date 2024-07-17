987d965d6d655cce106a2c496c4b1d405027e067
Nikolay Frolov <frolovne70@gmail.com>
2024-07-17 11:21:27 +0300

    Done. M1:task3-4

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
@@ -9,6 +9,14 @@ author = {
     "телефон": "+7 (917) 828 2232",
      "Email" : "frolov@bk.ru"
 }
+
+items = [
+   {"id": 1, "name": "Кроссовки abibas", "quantity":5},
+   {"id": 2, "name": "Куртка кожаная", "quantity":2},
+   {"id": 3, "name": "Coca-cola 1 литр", "quantity":12},
+   {"id": 4, "name": "Картофель фри", "quantity":0},
+   {"id": 5, "name": "Кепка", "quantity":124},
+]
 
 
 
@@ -28,3 +36,19 @@ def about(request):
     Email: {author["Email"]}<br>
     """
     return HttpResponse(text)
+
+# item/1
+# item/2
+# ...
+# item/n
+
+def get_item(request, item_id):
+    """ По указанному  id возвращает элемент из списка"""
+    for item in items:
+        if item['id'] == item_id:
+            result = f"""
+            <h2> Имя: {item["name"]} <h2>
+            <p> Количество: {item["quantity"]} <p>
+            """
+            return HttpResponse(result)
+    return HttpResponseNotFound(f"Товар с id={item_id} не найден)
