2949dee893b39e64e0637780723fc3ba83c1e2ec
Nikolay Frolov <frolovne70@gmail.com>
2024-07-18 12:31:13 +0300

    Add Item model and make migration

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,15 +2,15 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
-author = {
-    "Имя" : "Николай",
-    "Фамилия" : "Фролов",
-    "Отчество" : "Евгеньевич",
-    "телефон": "+7 (917) 828 2232",
-     "Email" : "frolov@bk.ru"
-}
+# author = {
+#     "Имя" : "Николай",
+#     "Фамилия" : "Фролов",
+#     "Отчество" : "Евгеньевич",
+#     "телефон": "+7 (917) 828 2232",
+#      "Email" : "frolov@bk.ru"
+# }
 
-items = [
+item = [
    {"id": 1, "name": "Кроссовки abibas", "quantity":5},
    {"id": 2, "name": "Куртка кожаная", "quantity":2},
    {"id": 3, "name": "Coca-cola 1 литр", "quantity":12},
@@ -32,6 +32,14 @@ def home(request):
     
 
 def about(request):
+    author = {
+    "name" : "Николай",
+    "middle_name" : "Фролов",
+    "Last_name" : "Евгеньевич",
+    "phone": "+7 (917) 828 2232",
+     "Email" : "frolov@bk.ru"
+}
+
     text = f"""
     Имя : {author["Имя"]}<br>
     Фамилия : {author["Фамилия"]}<br>
@@ -39,7 +47,7 @@ def about(request):
     телефон: {author["телефон"]}<br>
     Email: {author["Email"]}<br>
     """
-    return HttpResponse(text)
+    return render(request, "about.html", {"author" : author})
 
 # item/1
 # item/2
@@ -64,6 +72,7 @@ def get_items(request):
     #     result += f"""<li><a href ='/item/{item["id"]}'>{item["name"]}</li>"""
     # result += "</ol>"
     # return HttpResponse(result)
+    items = Item.objects.all()
     context = {
         "items" : items
     }
